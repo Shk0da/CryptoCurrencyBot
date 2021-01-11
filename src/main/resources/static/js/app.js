b130841b02828951286d4d1a87d93db7dd75c977
Anatoly.Shkondin <anatoly.shkondin@nordigy.ru>
2021-01-11 09:09:52 +0300

    update gradle

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -3,46 +3,53 @@ feather.replace();
 var chartWrapper = '<canvas id="cryptoChart" class="my-4" width="900" height="380"></canvas>';
 var app = angular.module("dashboard", [])
     .controller("mainCtr", function($scope, $interval, $http) {
-        $http.get("/api/markets").then(function(response) {
+
+        $scope.locaton = "";
+
+        function sleep(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
+
+        $http.get($scope.locaton + "/api/markets").then(function(response) {
             $scope.markets = response.data;
         });
 
-        $http.get("/api/trading/status").then(function(response) {
+        $http.get($scope.locaton + "/api/trading/status").then(function(response) {
             if (response.data == "on") {
                 $scope.power = "On"
                 $(".power").css({'color': '#99CC18'});
-             } else {
+            } else {
                 $scope.power = "Off"
                 $(".power").css({'color': 'red'});
             }
         });
 
         $scope.powerOnOff = function() {
-            $http.get("/api/trading?active=" + ($scope.power != "On")).then(function(response) {
+            $http.get($scope.locaton + "/api/trading?active=" + ($scope.power != "On")).then(function(response) {
                 if (response.data == "on") {
-                   $scope.power = "On"
-                   $(".power").css({'color': '#99CC18'});
+                    $scope.power = "On"
+                    $(".power").css({'color': '#99CC18'});
                 } else {
-                   $scope.power = "Off"
-                   $(".power").css({'color': 'red'});
+                    $scope.power = "Off"
+                    $(".power").css({'color': 'red'});
                 }
             }, function(error){console.log(error);}) ;
         }
 
         var logRefresh;
         $scope.showLog = function() {
-             $("main").addClass("d-none");
-             $("#logArea").removeClass("d-none");
-             $("#logArea").html("Loading, wait...");
+            $("main").addClass("d-none");
+            $("#logArea").removeClass("d-none");
+            $("#logArea").html("Loading, wait...");
 
-             logRefresh = $interval(function() {
-                 $http.get("/api/logging").then(function(response) {
-                     $("#logArea").html("<pre>" + response.data.replace("\n", "<br />", "g") + "</pre>");
-                 });
-             }, 2000);
+            logRefresh = $interval(function() {
+                $http.get($scope.locaton + "/api/logging").then(function(response) {
+                    $("#logArea").html("<pre>" + response.data.replace("\n", "<br />", "g") + "</pre>");
+                });
+            }, 2000);
         }
 
-        $scope.showMarket = function(event) {
+        $scope.showMarket = async function (event) {
             if (angular.isDefined(logRefresh)) {
                 $interval.cancel(logRefresh);
                 logRefresh = undefined;
@@ -55,17 +62,20 @@ var app = angular.module("dashboard", [])
             $scope.AccountInfo = "Loading, wait...";
             $(".chartWrapper").html("");
             $(".accinf").addClass("d-none");
-            $http.get("/api/status?market=" + market).then(function(response) {
+            $http.get($scope.locaton + "/api/status?market=" + market).then(function (response) {
                 $scope.AccountInfo = response.data;
                 $(".accinf").removeClass("d-none");
             });
-            $http.get("/api/symbols?market=" + market).then(function(response) {
+            await sleep(1000);
+            $http.get($scope.locaton + "/api/symbols?market=" + market).then(function (response) {
                 $scope.symbols = response.data;
             });
-            $http.get("/api/orders/active?limit=20&market=" + market).then(function(response) {
+            await sleep(1000);
+            $http.get($scope.locaton + "/api/orders/active?limit=20&market=" + market).then(function (response) {
                 $scope.activeOrders = response.data;
             });
-            $http.get("/api/orders/history?limit=20&market=" + market).then(function(response) {
+            await sleep(1000);
+            $http.get($scope.locaton + "/api/orders/history?limit=20&market=" + market).then(function (response) {
                 $scope.historyOrders = response.data;
                 $("#logArea").addClass("d-none");
                 $("main").removeClass("d-none");
